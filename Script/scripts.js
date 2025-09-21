e73558d4ccdb4b15b2a40e4bca83e92a18879c52
Ilhaam Imam <108522726+ilhaamimam@users.noreply.github.com>
2025-09-22 02:12:01 +0530

    Add files via upload

--- Script/scripts.js
+++ Script/scripts.js
@@ -398,3 +398,15 @@ faqQuestions.forEach(btn => {
     answer.style.display = answer.style.display === "block" ? "none" : "block";
   });
 });
+
+
+
+//pwa workspace
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("✅ Service Worker registered:", reg))
+      .catch((err) => console.log("❌ Service Worker failed:", err));
+  });
+}
